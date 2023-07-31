26d98a94ced1b47990f5630a4df01cc1d147b4c5
Natalitta <nataliialebedieva@gmail.com>
2023-07-31 18:28:13 +0000

    Add env variables to connect to database and heroku

---
+++  env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://lyudcckk:2-RUWuO7jVtnoHgjBMGVNv4hLLxws5Mk@trumpet.db.elephantsql.com/lyudcckk"
+os.environ["SECRET_KEY"] = "^&+nrhs*vy6vlo1uw@ho+i!g06vpmzkyd5e^lm0=@n72$aa1v7"
